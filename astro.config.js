3baccab152c5f71efd4d7ded9850bfe7382bdc3c
osvaldo9007 <osvaldo9007@gmail.com>
2024-01-17 08:56:28 -0500

    Add new fonts and update tsconfig.json and package.json

--- astro.config.js
+++ astro.config.js
@@ -1,8 +1,8 @@
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  trailingSlash: "always",
+  trailingSlash: 'always',
   integrations: [mdx(), tailwind()],
 });
